605cd98158d55fae179e4a0af8a5dda439272403
Fabricio Silva <hi@fabricio.dev>
2025-03-25 19:18:31 +0000

    add filter to products-list

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -3,38 +3,44 @@ import { Component, DestroyRef, inject, OnInit } from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import {
+  BehaviorSubject,
   combineLatest,
   debounceTime,
   distinctUntilChanged,
   map,
   Observable,
 } from 'rxjs';
+import { ProductsSearchPipe } from '../../pipes/products-search.pipe';
 import { Product, ProductsService } from '../../services/products.service';
 
 export interface ViewModel {
   products: Product[];
+  searchTerm: string;
 }
 
 @Component({
   selector: 'app-product-list',
-  imports: [ReactiveFormsModule, AsyncPipe, CurrencyPipe],
+  imports: [ReactiveFormsModule, AsyncPipe, CurrencyPipe, ProductsSearchPipe],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.scss',
 })
 export class ProductListComponent implements OnInit {
   private _destroyRef = inject(DestroyRef);
   private _productService = inject(ProductsService);
+  private _searchTerm$ = new BehaviorSubject('');
 
   public formGroup = new FormGroup({
-    term: new FormControl(''),
+    term: new FormControl('', { nonNullable: true }),
   });
 
   public vm$: Observable<ViewModel> = combineLatest([
     this._productService.products$,
+    this._searchTerm$,
   ]).pipe(
     takeUntilDestroyed(this._destroyRef),
-    map(([products]) => ({
+    map(([products, searchTerm]) => ({
       products,
+      searchTerm,
     }))
   );
 
@@ -51,7 +57,7 @@ export class ProductListComponent implements OnInit {
         debounceTime(600)
       )
       .subscribe((term) => {
-        console.log(term);
+        this._searchTerm$.next(term);
       });
   }
 }
--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -19,17 +19,27 @@ export class ProductsService {
       {
         id: '21b362d7-097b-4cd3-b654-7971c4b5306c',
         name: 'Hair brush',
-        price: 200,
+        price: 100,
+      },
+      {
+        id: 'd2863dde-77a2-404e-9bd2-f3f1a4fcdd9d',
+        name: 'Hair brush black',
+        price: 340,
+      },
+      {
+        id: '917c9d76-ab7f-47fd-8b14-3b71fe7cc8d3',
+        name: 'Hair dryer',
+        price: 210,
       },
       {
         id: 'bb363ae2-da6d-4400-b653-eb3b7345926e',
-        name: 'Hair brush',
+        name: 'Hair dryer white',
         price: 200,
       },
       {
         id: 'e261c6f0-a3c1-4483-86bc-6d156a51e0a7',
-        name: 'Hair brush',
-        price: 200,
+        name: 'Hair straighteners',
+        price: 250,
       },
     ];
 
