d68702345fe493ac367621c0a7c2f5069a729159
Fabricio Silva <hi@fabricio.dev>
2025-03-25 23:05:56 +0000

    create some tests for home

--- src/app/pages/home/home.component.spec.ts
+++ src/app/pages/home/home.component.spec.ts
@@ -1,22 +1,54 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { of } from 'rxjs';
+import { ProductsService } from '../../services/products.service';
 import { HomeComponent } from './home.component';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
+  let valueServiceSpy: jasmine.SpyObj<ProductsService>;
 
   beforeEach(async () => {
+    const spy = jasmine.createSpyObj('ProductsService', ['getProducts']);
     await TestBed.configureTestingModule({
       imports: [HomeComponent],
+      providers: [
+        {
+          provide: ProductsService,
+          useValue: spy,
+        },
+      ],
     }).compileComponents();
+
+    valueServiceSpy = TestBed.inject(
+      ProductsService
+    ) as jasmine.SpyObj<ProductsService>;
+
+    valueServiceSpy.products$ = of([]);
+    valueServiceSpy.getProducts.and.returnValue(of([]));
 
     fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
+    fixture.detectChanges();
     expect(component).toBeTruthy();
+  });
+
+  it('should render app-product-list', () => {
+    fixture.detectChanges();
+    console.log(fixture.nativeElement);
+    expect(
+      fixture.nativeElement.querySelector('app-product-list')
+    ).toBeTruthy();
+  });
+
+  it('should call #getProducts on init', () => {
+    fixture.detectChanges();
+    component.ngOnInit();
+
+    expect(valueServiceSpy.getProducts).toHaveBeenCalled();
   });
 });
