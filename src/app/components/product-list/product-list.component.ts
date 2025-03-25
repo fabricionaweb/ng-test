46410904e7fe30478d7fde3d2302a3519f058755
Fabricio Silva <hi@fabricio.dev>
2025-03-25 21:03:48 +0000

    move business logic out of product-list

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,17 +1,7 @@
-import { AsyncPipe, CurrencyPipe } from '@angular/common';
-import { Component, DestroyRef, inject, OnInit } from '@angular/core';
-import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
-import {
-  BehaviorSubject,
-  combineLatest,
-  debounceTime,
-  distinctUntilChanged,
-  map,
-  Observable,
-} from 'rxjs';
-import { ProductsSearchPipe } from '../../pipes/products-search.pipe';
-import { Product, ProductsService } from '../../services/products.service';
+import { CurrencyPipe } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { Product } from '../../services/products.service';
 
 export interface ViewModel {
   products: Product[];
@@ -20,44 +10,10 @@ export interface ViewModel {
 
 @Component({
   selector: 'app-product-list',
-  imports: [ReactiveFormsModule, AsyncPipe, CurrencyPipe, ProductsSearchPipe],
+  imports: [ReactiveFormsModule, CurrencyPipe],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.scss',
 })
-export class ProductListComponent implements OnInit {
-  private _destroyRef = inject(DestroyRef);
-  private _productService = inject(ProductsService);
-  private _searchTerm$ = new BehaviorSubject('');
-
-  public formGroup = new FormGroup({
-    term: new FormControl('', { nonNullable: true }),
-  });
-
-  public vm$: Observable<ViewModel> = combineLatest([
-    this._productService.products$,
-    this._searchTerm$,
-  ]).pipe(
-    takeUntilDestroyed(this._destroyRef),
-    map(([products, searchTerm]) => ({
-      products,
-      searchTerm,
-    }))
-  );
-
-  ngOnInit(): void {
-    this._productService
-      .getProducts()
-      .pipe(takeUntilDestroyed(this._destroyRef))
-      .subscribe();
-
-    this.formGroup.controls.term.valueChanges
-      .pipe(
-        takeUntilDestroyed(this._destroyRef),
-        distinctUntilChanged(),
-        debounceTime(600)
-      )
-      .subscribe((term) => {
-        this._searchTerm$.next(term);
-      });
-  }
+export class ProductListComponent {
+  @Input() products!: Product[];
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,67 @@
-import { Component } from '@angular/core';
-import { ProductListComponent } from '../../components/product-list/product-list.component';
+import { AsyncPipe } from '@angular/common';
+import { Component, DestroyRef, inject, OnInit } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import {
+  BehaviorSubject,
+  combineLatest,
+  debounceTime,
+  distinctUntilChanged,
+  map,
+  Observable,
+} from 'rxjs';
+import {
+  ProductListComponent,
+  ViewModel,
+} from '../../components/product-list/product-list.component';
+import { ProductsSearchPipe } from '../../pipes/products-search.pipe';
+import { ProductsService } from '../../services/products.service';
 
 @Component({
   selector: 'app-home',
-  imports: [ProductListComponent],
+  imports: [
+    ProductListComponent,
+    ReactiveFormsModule,
+    AsyncPipe,
+    ProductsSearchPipe,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit {
+  private _destroyRef = inject(DestroyRef);
+  private _productService = inject(ProductsService);
+  private _searchTerm$ = new BehaviorSubject('');
+
+  public formGroup = new FormGroup({
+    term: new FormControl('', { nonNullable: true }),
+  });
+
+  public vm$: Observable<ViewModel> = combineLatest([
+    this._productService.products$,
+    this._searchTerm$,
+  ]).pipe(
+    takeUntilDestroyed(this._destroyRef),
+    map(([products, searchTerm]) => ({
+      products,
+      searchTerm,
+    }))
+  );
+
+  ngOnInit(): void {
+    this._productService
+      .getProducts()
+      .pipe(takeUntilDestroyed(this._destroyRef))
+      .subscribe();
+
+    this.formGroup.controls.term.valueChanges
+      .pipe(
+        takeUntilDestroyed(this._destroyRef),
+        distinctUntilChanged(),
+        debounceTime(600)
+      )
+      .subscribe((term) => {
+        this._searchTerm$.next(term);
+      });
+  }
+}
